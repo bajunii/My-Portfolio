b9f7760677b4493f659a0a83380741e6e5d1892d
bajunii <haithamomar520@gmail.com>
2025-06-25 19:22:39 +0300

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ const projects: Project[] = [
     technologies: ['React', 'Node.js', 'MongoDB', 'Stripe API'],
     githubLink: 'https://github.com/bajunii/fitflow',
     liveLink: 'https://fitflow-demo.com',
-    image: 'public/assets/fitness.jpeg'
+    image: '/fitness.jpeg'
   },
   {
     id: 2,
@@ -47,7 +47,7 @@ const projects: Project[] = [
     technologies: ['React', 'TypeScript', 'Tailwind CSS'],
     githubLink: 'https://github.com/bajunii/My-Portfolio',
     liveLink: 'https://my-portfolio-khaki-seven-67.vercel.app/',
-    image: 'public/assets/portfolio.png'
+    image: '/portfolio.png'
   },
   {
     id: 3,
@@ -56,7 +56,7 @@ const projects: Project[] = [
     technologies: ['React','Tailwind CSS', 'Typescript', 'Vite'],
     githubLink: 'https://github.com/mikesplore/swahilipot-hub-foundation',
     liveLink: 'https://www.swahilipothub.co.ke/',
-    image: 'public/assets/swahilipot.png'
+    image: '/swahilipot.png'
   },
 ];
 const education: Education[] = [
@@ -215,7 +215,7 @@ const App = () => {
           </div>
           <div className="hero-image">
             <img 
-            src='public\assets\about.jpg'
+            src='/about.jpg'
               alt="Profile Photo" 
               className="profile-photo"
             />
@@ -246,7 +246,7 @@ const App = () => {
             </div>
             <div className="about-image">
               <img 
-               src='public\assets\haitham.jpg'
+               src='/haitham.jpg'
                 alt="About me illustration" 
               />
             </div>
